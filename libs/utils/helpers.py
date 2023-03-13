d1dc6e36bea8c56df5bd9a53839fb6f308e17173
levuloihust99 <49064246+levuloihust99@users.noreply.github.com>
2023-03-13 17:19:50 +0700

    add pandas parser

---
+++ libs/utils/helpers.py
@@ -0,0 +1,17 @@
+def recursive_apply(data, fn):
+    stack = [(None, -1, data)]  # parent, idx, child: parent[idx] = child
+    while stack:
+        parent_node, index, node = stack.pop()
+        if isinstance(node, list):
+            stack.extend(list(zip([node] * len(node), range(len(node)), node)))
+        elif isinstance(node, dict):
+            stack.extend(
+                list(zip([node] * len(node), node.keys(), node.values())))
+        elif isinstance(node, str):
+            if node:
+                parent_node[index] = fn(node)
+            else:
+                parent_node[index] = node
+        else:
+            continue
+    return data
