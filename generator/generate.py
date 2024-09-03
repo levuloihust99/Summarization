913a11b88fc898ece3b52c7d0ff86079534e014e
levuloihust99 <levuloihust@gmail.com>
2024-09-03 17:53:58 +0700

    sequential write for generator.generate

--- generator/generate.py
+++ generator/generate.py
@@ -52,15 +52,54 @@ def main():
     batch = []
     batch_size = args.batch_size
     progress_bar = tqdm(total=len(data), desc="Processing")
-    for item in data:
-        if len(batch) == batch_size:
+
+    processed = set()
+    if os.path.exists(args.output_path):
+        with open(args.output_path, "r", encoding="utf-8") as reader:
+            for line in reader:
+                item = json.loads(line.strip())
+                processed.add(item[args.id_key])
+
+    with open(args.output_path, "a", encoding="utf-8") as writer:
+        for item in data:
+            if item[args.id_key] in processed:
+                progress_bar.update(1)
+                continue
+            batch.append(item)
+            if len(batch) == batch_size:
+                if args.sampler == "top10_30_sampler":
+                    range_lower = max(50, args.max_length - 100)
+                    range_upper = max(args.max_length, range_lower + 50)
+                    selected_max_len = random.randint(range_lower, range_upper)
+                    kwargs.update(max_length=selected_max_len)
+                else:
+                    kwargs.update(max_length=args.max_length)
+                outputs = summarizer.greedy(
+                    [_item[args.input_key] for _item in batch],
+                    block_n_grams=args.block_n_grams,
+                    **kwargs
+                )
+                done_batch = []
+                for output, _item in zip(outputs, batch):
+                    done_batch.append({
+                        args.id_key: _item[args.id_key],
+                        args.input_key: _item[args.input_key],
+                        args.output_key: output
+                    })
+                    progress_bar.update(1)
+                out_data.extend(done_batch)
+                for done_item in done_batch:
+                    writer.write(json.dumps(done_item, ensure_ascii=False) + "\n")
+                    writer.flush()
+                batch = []
+
+        if len(batch) > 0:
+            logger.info("Last batch...")
             if args.sampler == "top10_30_sampler":
                 range_lower = max(50, args.max_length - 100)
                 range_upper = max(args.max_length, range_lower + 50)
                 selected_max_len = random.randint(range_lower, range_upper)
                 kwargs.update(max_length=selected_max_len)
-            else:
-                kwargs.update(max_length=args.max_length)
             outputs = summarizer.greedy(
                 [_item[args.input_key] for _item in batch],
                 block_n_grams=args.block_n_grams,
@@ -73,32 +112,6 @@ def main():
                     args.output_key: output
                 })
                 progress_bar.update(1)
-            batch = []
-        batch.append(item)
-
-    if len(batch) > 0:
-        logger.info("Last batch...")
-        if args.sampler == "top10_30_sampler":
-            range_lower = max(50, args.max_length - 100)
-            range_upper = max(args.max_length, range_lower + 50)
-            selected_max_len = random.randint(range_lower, range_upper)
-            kwargs.update(max_length=selected_max_len)
-        outputs = summarizer.greedy(
-            [_item[args.input_key] for _item in batch],
-            block_n_grams=args.block_n_grams,
-            **kwargs
-        )
-        for output, _item in zip(outputs, batch):
-            out_data.append({
-                args.id_key: _item[args.id_key],
-                args.input_key: _item[args.input_key],
-                args.output_key: output
-            })
-            progress_bar.update(1)
-
-    with open(args.output_path, "w") as writer:
-        for item in tqdm(out_data, desc="Writing"):
-            writer.write(json.dumps(item, ensure_ascii=False) + "\n")
 
 
 if __name__ == "__main__":
